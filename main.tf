f91ef175215b3d9043769f46c067dabe8d389c0f
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-12-10 14:52:53 +0100

    add twilio auth token for webhook validation

--- main.tf
+++ main.tf
@@ -22,6 +22,12 @@ variable "twilio_api_key_secret" { sensitive = true }
 variable "twilio_phone_number" {}
 variable "twilio_account_sid" {}
 
+variable "twilio_auth_token" { sensitive = true }
+variable "twilio_webhook_root_url" {
+  type = string
+  description = "The root URL to send Twilio webhooks to"
+}
+
 data "aws_region" "current" {
 }
 
@@ -33,6 +39,12 @@ resource "aws_ssm_parameter" "api_key_secret" {
   name  = "/twilio_client/${random_pet.pkg_id.id}/api_key_secret"
   type  = "SecureString"
   value = var.twilio_api_key_secret
+}
+
+resource "aws_ssm_parameter" "auth_token" {
+  name  = "/twilio_client/${random_pet.pkg_id.id}/auth_token"
+  type  = "SecureString"
+  value = var.twilio_auth_token
 }
 
 output "TWILIO_API_KEY_SID" {
@@ -48,12 +60,25 @@ output "TWILIO_API_KEY_SECRET" {
   }
 }
 
+output "TWILIO_AUTH_TOKEN" {
+  value = {
+    type   = "ssm"
+    key    = aws_ssm_parameter.auth_token.name
+    region = data.aws_region.current.name
+    arn    = aws_ssm_parameter.auth_token.arn
+  }
+}
+
 output "TWILIO_PHONE_NUMBER" {
   value = var.twilio_phone_number
 }
 
 output "TWILIO_ACCOUNT_SID" {
   value = var.twilio_account_sid
+}
+
+output "TWILIO_WEBHOOK_ROOT_URL" {
+  value = var.twilio_webhook_root_url
 }
 
 
